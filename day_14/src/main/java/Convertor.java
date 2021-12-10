fa63437ef5962e205747de7929f61180fc0aa773
Nithiya Paul <nithiya.paul@mca.christuniversity.in>
2021-12-10 09:17:18 +0530

    created test cases fot convertor opertion

--- day_14/src/main/java/Convertor.java
+++ day_14/src/main/java/Convertor.java
@@ -1,52 +1,44 @@
 public class Convertor {
 
 
-    public double unitConvertor(double measurement, String fromUnit, String toUnit){
-        if(measurement<= 0){
+    public double unitConvertor(double measurement, String fromUnit, String toUnit) {
+        if (measurement <= 0) {
             throw new IllegalArgumentException("Measurement can't be less than or equal to zero");
-        }
-
-        else {
-            if(toUnit=="cm"){
-                return   convertToCentimeter(measurement, fromUnit);
-            }
-            else if(toUnit=="km"){
-                return  convertToKilometer(measurement, fromUnit);
-            }
-            else {
+        } else {
+            if (toUnit == "cm") {
+                return convertToCentimeter(measurement, fromUnit);
+            } else if (toUnit == "km") {
+                return convertToKilometer(measurement, fromUnit);
+            } else {
                 throw new IllegalArgumentException("Not a valid unit");
             }
         }
-
 
     }
 
     public double convertToCentimeter(double measurement, String fromUnit) {
         if (fromUnit == "cm") {
-
             return measurement;
-
-        }
-        else if(fromUnit=="m"){
+        } else if (fromUnit == "m") {
             return measurement * 100;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Not a valid unit");
         }
-
     }
 
     public double convertToKilometer(double measurement, String fromUnit) {
         if (fromUnit == "cm") {
-
             return measurement / 100000;
-
-        }
-
-        else {
+        } else {
             throw new IllegalArgumentException("Not a valid unit");
         }
+    }
+    public double add(double measure1, String unit1, double measure2, String unit2 ){
+        return unitConvertor(measure1,unit1,unit1) + unitConvertor(measure2,unit2,unit1);
+    }
 
+    public double subtract(double measure1, String unit1, double measure2, String unit2 ){
+        return unitConvertor(measure1,unit1,unit1) - unitConvertor(measure2,unit2,unit1);
     }
 
 }
--- day_14/src/test/java/CovertorTest.java
+++ day_14/src/test/java/CovertorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,6 +53,51 @@ class ConvertorTest {
         assertThrows(IllegalArgumentException.class, ()-> {
             convertor.unitConvertor(1, "km","km");
         });
+    }
+
+    @Nested
+    public class ArithmaticOperationsOnUnits{
+        @Test
+        public void checkIfOneMeterPlusHundresCentimeterIsTwoMeter(){
+            double actualMeasurement= convertor.add(1,"m",100,"cm");
+            double expectedMeasurement = 2;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
+
+        @Test
+        public void checkIfOneMeterMinusFiftyCentimeterIsZeroPointFiveMeterMeter(){
+            double actualMeasurement= convertor.subtract(1,"m",50,"cm");
+            double expectedMeasurement = 0.5;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
+
+        @Test
+        public  void checkIfTwoHundredCentimeterPlusOneKilometerIsHundredThousandTwoHundredCentimeter(){
+            double actualMeasurement = convertor.add(200,"cm",1,"km");
+            double expectedMeasurement = 100200;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
+
+        @Test
+        public void checkIfTwoThousandCentimeterMinusOneMeterIsNinteenHundredCentimeter(){
+            double actualMeasurement= convertor.subtract(2000,"cm",1,"m");
+            double expectedMeasurement = 1900;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
+
+        @Test
+        public void checkIfFourThousandMeterMinuseTwoMeterIsTwoThousandMeter(){
+            double actualMeasurement= convertor.subtract(4000,"m",2,"km");
+            double expectedMeasurement = 2000;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
+
+        @Test
+        public void checkIf4KilometerMinuseTwoKilometerIs2Kilometer(){
+            double actualMeasurement= convertor.subtract(4,"km",2,"km");
+            double expectedMeasurement = 2;
+            assertEquals(expectedMeasurement,actualMeasurement);
+        }
     }
 
 
